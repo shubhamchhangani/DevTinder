35e57723b954d6c856fa1281dd8d333aec55823e
Shubham Chhangani <shubhamchhangani1998@gmail.com>
2025-02-23 17:05:27 +0530

    second trial commit

---
+++ src/app.js
@@ -0,0 +1,9 @@
+console.log("hello");
+const express = require('express');
+const app = express();
+app.use("/", (req, res) => {
+    res.send("hello from server");
+})
+app.listen("3000", () => {
+    console.log("server is running on port:3000");
+});
