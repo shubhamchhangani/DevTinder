133940449cd01aebbe97402ba8f249b2f9f15712
Shubham Chhangani <shubhamchhangani1998@gmail.com>
2025-02-23 21:48:44 +0530

    multiple request handlers

--- src/app.js
+++ src/app.js
@@ -1,9 +1,25 @@
 console.log("hello");
 const express = require('express');
 const app = express();
+app.get("/getUser", (req,res) => {
+    res.send({
+        name: "Shubham",
+        city: "Pokaran"
+    })
+})
+app.get("/user", (req, res, next) => {
+    next();
+    //res.send("first request handler");
+}, (req, res, next) => {
+    //next();
+    res.send("second request handler");
+}, (req, res, next) => {
+    //next();
+    //res.send("third request handler");
+})
 app.use("/", (req, res) => {
     res.send("hello from server");
 })
 app.listen("3000", () => {
-    console.log("server is running on port:3000");
+    console.log("server is running on port:3000"); 
 });
