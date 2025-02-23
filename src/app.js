3cbc0de9efd20bbcb595e579589434c9b9f1dbd4
Shubham Chhangani <shubhamchhangani1998@gmail.com>
2025-02-23 23:26:36 +0530

    trying error handling

--- src/app.js
+++ src/app.js
@@ -1,17 +1,27 @@
 console.log("hello");
-const express = require('express');
-const { adminAuth, userAuth } = require('./middlewares/auth');
+const express = require("express");
+const { adminAuth, userAuth } = require("./middlewares/auth");
 const app = express();
 
 app.use("/admin", adminAuth);
 app.get("/admin/getAllUsers", (req, res) => {
-    res.send("got the data of all the users");
-})
+  res.send("got the data of all the users");
+});
 
 app.get("/user", userAuth, (req, res) => {
-    res.send("data of user fetched");
-})
+  console.log("inside user api");
+  //throw new Error("Error");
+  res.send("data of user fetched");
+});
+
+app.use("/", (err, req, res, next) => {
+  if (err) {
+    res.send("something went wrong contact support");
+  } else {
+    next();
+  }
+});
 
 app.listen("3000", () => {
-    console.log("server is running on port:3000"); 
+  console.log("server is running on port:3000");
 });
