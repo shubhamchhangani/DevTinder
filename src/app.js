5fc1bcd6e9bf499c6208e66d599bbb9881fffbab
Shubham Chhangani <shubhamchhangani1998@gmail.com>
2025-03-03 12:20:35 +0530

    update and delete apis

--- src/app.js
+++ src/app.js
@@ -1,8 +1,86 @@
 console.log("hello");
 const express = require("express");
+const User = require("./models/user");
 const app = express();
 const connect = require("./config/database");
 connect();
+
+app.use(express.json());
+
+app.post("/signup", async (req, res) => {
+    const newUser = req.body;
+    //console.log(newUser);
+    const user = new User(newUser);
+    await user.save();
+    res.send("new user created");
+})
+
+app.get("/users", async (req, res) => {
+  const userEmail = req.body.email;
+  //console.log(userEmail);
+  try {
+    
+    const users = await User.find({email : userEmail});
+    if(users.length === 0){
+      res.status(404).send("user not found");
+    }else{
+      res.send(users);
+    }
+  } catch (error) {
+    res.status(400).send(error);
+  }
+})
+
+app.get("/feed", async (req, res) => {
+  try {
+    res.send(await User.find({}));
+  } catch (error) {
+    res.status(400).send("something went wrong");
+  }
+})
+
+app.get("/user", async (req, res) => {
+  const userEmail = req.body.email;
+  try {
+    const user = await  User.findOne({email : userEmail});
+    if(!user){
+      res.status(404).send("user not found");
+    }else{
+      res.send(user);
+    }
+  } catch (error) {
+    res.status(400).send("Something went wrong");
+  }
+})
+
+//delete the user
+
+app.delete("/user", async (req, res) => {
+  const userId = req.body.userId;
+  try {
+    const user = await User.findByIdAndDelete(userId);
+    res.send("user deleted successfully")
+  } catch (error) {
+    res.status(400).send("Something went wrong");
+  }
+})
+
+//update the user
+
+app.patch("/user", async (req, res) => {
+  const userId = req.body.userId;
+  const data = req.body;
+  console.log(userId);
+  console.log(data);
+  try {
+    const user = await User.findByIdAndUpdate(userId, data);
+    res.send("user updated successfully")
+  } catch (error) {
+    res.status(400).send("something went wrong");
+  }
+})
+
+
 app.listen("3000", () => {
   console.log("server is running on port:3000");
 });
---
+++ src/models/user.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+const userScheme = new mongoose.Schema(
+    {
+        firstName: {
+            type: String
+        },
+        lastName: {
+            type: String
+        },
+        email: {
+            type: String
+        },
+        password: {
+            type: String
+        },
+        age: {
+            type: Number
+        },
+        gender: {
+            type: String
+        }
+    }
+)
+
+const User = mongoose.model("User", userScheme);
+module.exports = User;
